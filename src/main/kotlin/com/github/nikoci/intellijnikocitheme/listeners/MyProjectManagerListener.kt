bc9256762e9ebf586f1c11bd33acc3cc9efbf199
GitHub Action <action@github.com>
2023-01-16 12:37:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/nikoci/intellijnikocitheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nikoci/intellijnikocitheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nikoci.intellijnikocitheme.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nikoci.intellijnikocitheme.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/nikoci/intellijnikocitheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nikoci/intellijnikocitheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nikoci.intellijnikocitheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nikoci.intellijnikocitheme.MyBundle
 
 class MyApplicationService {
 
